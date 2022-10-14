815cd9c1cdbde766edf7ac85a097baf7c34ffb1f
NEWJYH <joung5846@naver.com>
2022-10-14 12:06:58 +0900

    Edit App.js git add .git add .git add .!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO WORLD
+          HELLO WORLD SHOW SHOW SHOW
           정영훈 <code>src/App.js</code> and save to reload.
         </p>
       </header>
