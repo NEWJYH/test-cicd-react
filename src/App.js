a7209197a6ca21bf744bbb13914047928f35a06a
NEWJYH <joung5846@naver.com>
2022-10-19 15:36:32 +0900

    just test pipLine for Jenkins

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TodoApp_NEWJYH from './TodoApp_NEWJYH';
 function App() {
   return (
     <div className="App">
-      <TodoApp_NEWJYH /><h1>15:01</h1>
+      <TodoApp_NEWJYH /><h1>15:36</h1>
           Learn React
     </div>
   );
